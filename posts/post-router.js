21c68bb8693aa5d3b029c972e4f57237dc3c64e0
BradenEM <bradenemason@gmail.com>
2019-08-06 22:36:23 -0500

    edited status code

--- posts/post-router.js
+++ posts/post-router.js
@@ -58,7 +58,7 @@ router.delete('/:id', async (req, res) => {
 		const {id} = req.params;
 		const post = await db('posts').where('id', '=', id).del();
 
-		res.status(204).json(post)
+		res.status(200).json(post)
 	} catch (error) {
 		res.status(500).json(error);
 	}
