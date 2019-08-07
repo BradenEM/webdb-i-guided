8ae2fd5661910bd24eda7967de1bfefa10e8ea03
BradenEM <bradenemason@gmail.com>
2019-08-06 22:30:28 -0500

    guided project up to date

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,23 +5,63 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+	const posts = await db('posts');
+	// const posts = await db.select('*').from('posts'); EXAMPLE
+	try {
+		const posts = await db('posts');
+	// const posts = await db.select('*').from('posts'); EXAMPLE
+		res.status(200).json(posts);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
+
+router.get('/:id', async (req, res) => {
+try {
+	const {id} = req.params;
+	const [post] = await db('posts').where({id});
+
+	res.status(200).json(post);
+} catch (error) {
+	res.status(500).json(error);
+}
+});
+
+router.post('/', async (req, res) => {
+	try {
+		const postData = req.body;
+		const post = await db('posts').insert(postData);
+
+		res.status(201).json(post);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 
 });
 
-router.get('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+	try {
+		const {id} = req.params;
+		const postData = req.body;
+		const count = await db('posts').where('id', '=', id).update(postData);
+
+		res.status(200).json(count);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 
 });
 
-router.post('/', (req, res) => {
+router.delete('/:id', async (req, res) => {
+	try {
+		const {id} = req.params;
+		const post = await db('posts').where('id', '=', id).del();
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
+		res.status(204).json(post)
+	} catch (error) {
+		res.status(500).json(error);
+	}
 
 });
 
